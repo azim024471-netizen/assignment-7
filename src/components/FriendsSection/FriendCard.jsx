d50ff559d75d91a7f7463287e385c276fedcaba5
azim024471-netizen <azim024471@gmail.com>
2026-04-16 02:41:13 +0600

    Completed the friends section and added card

---
+++ src/components/FriendsSection/FriendCard.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+const FriendCard = ({ friend }) => {
+    const { name, picture, days_since_contact, status, tags } = friend;
+
+    
+
+    return (
+        <div className="bg-white rounded-lg p-6 shadow-sm flex flex-col items-center text-center border border-gray-100">
+            
+            
+            
+            <div className="space-y-2">
+                <h4 className="text-[20px] font-semibold ">{name}</h4>
+            <p className="text-xs text-gray-500 ">{days_since_contact}d ago</p>
+
+        
+            <div className="flex flex-wrap justify-center gap-2 ">
+                {tags.map((tag, index) => (
+                    <span 
+                        key={index} 
+                        className="bg-green-200 text-green-700 text-xs font-medium px-3 py-1.5 rounded-full "
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+
+            <div>
+                <span className={`text-xs font-medium px-3 py-1.5 rounded-full text-white
+                 ${status === "Overdue" ? "bg-[#EF4444]" :
+                    status === "Almost Due" ? "bg-[#EFAD44]" :
+                    status === "On-Track" ? "bg-green-900" : ""
+
+                } `}>{status}</span>
+            </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default FriendCard;
--- src/components/FriendsSection/FriendsSection.jsx
+++ src/components/FriendsSection/FriendsSection.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import FriendCard from "./FriendCard";
 
-const FriendsSection = async() => {
-//     const res = await fetch()
-//     const friendsData = await res.json()
-//    console.log(friendsData)
+const FriendsSection = async () => {
+
+    const res = await fetch("http://localhost:3000/friendsData.json");
+    const friendsData = await res.json()
+    console.log(friendsData)
+
+
     return (
-        <div className='h-screen bg-amber-700 container mx-auto'>
-            <h3 className='font-semibold text-2xl'>Your Friends</h3>
+        <div className='h-screen  container mx-auto'>
+            <h3 className='font-semibold text-2xl mb-4 text-center md:text-left'>Your Friends</h3>
 
-            <div>
-
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {
+                  friendsData.map(friend=> <FriendCard key={friend.id} friend={friend}> </FriendCard>)
+                }
             </div>
         </div>
     );
